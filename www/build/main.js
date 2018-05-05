2cd73031d2b34fefe0a8a9ae2036b6cf1e89fb40
freddamour <fdamour121@gmail.com>
2018-05-06 01:16:51 +0200

    ajout de propriétés au pdf

--- www/build/main.js
+++ www/build/main.js
@@ -1,6 +1,6 @@
-webpackJsonp([0],{
+webpackJsonp([1],{
 
-/***/ 157:
+/***/ 169:
 /***/ (function(module, exports) {
 
 function webpackEmptyAsyncContext(req) {
@@ -13,11 +13,11 @@ function webpackEmptyAsyncContext(req) {
 webpackEmptyAsyncContext.keys = function() { return []; };
 webpackEmptyAsyncContext.resolve = webpackEmptyAsyncContext;
 module.exports = webpackEmptyAsyncContext;
-webpackEmptyAsyncContext.id = 157;
+webpackEmptyAsyncContext.id = 169;
 
 /***/ }),
 
-/***/ 202:
+/***/ 214:
 /***/ (function(module, exports) {
 
 function webpackEmptyAsyncContext(req) {
@@ -30,18 +30,18 @@ function webpackEmptyAsyncContext(req) {
 webpackEmptyAsyncContext.keys = function() { return []; };
 webpackEmptyAsyncContext.resolve = webpackEmptyAsyncContext;
 module.exports = webpackEmptyAsyncContext;
-webpackEmptyAsyncContext.id = 202;
+webpackEmptyAsyncContext.id = 214;
 
 /***/ }),
 
-/***/ 248:
+/***/ 260:
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return HomePage; });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(1);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_ionic_angular__ = __webpack_require__(88);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_rxjs__ = __webpack_require__(389);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_ionic_angular__ = __webpack_require__(96);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_rxjs__ = __webpack_require__(411);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_rxjs___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_2_rxjs__);
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
@@ -111,25 +111,24 @@ var HomePage = /** @class */ (function () {
     };
     HomePage = __decorate([
         Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["m" /* Component */])({
-            selector: 'page-home',template:/*ion-inline-start:"C:\Users\eea\Documents\GitHub\01 vue\02_V2B_ionic_start_V1\src\pages\home\home.html"*/'<ion-header>\n\n  <ion-navbar>\n\n    <ion-title>\n\n      Ionic Blank\n\n    </ion-title>\n\n  </ion-navbar>\n\n</ion-header>\n\n\n\n<ion-content padding>\n\n\n\n    <div>\n\n        <label>PDF src</label>\n\n        <input type="text" placeholder="PDF src" [(ngModel)]="pdfSrc">\n\n    </div>\n\n    <pdf-viewer [src]="pdfSrc" \n\n                [render-text]="true"\n\n                style="display: block;"\n\n    ></pdf-viewer>\n\n\n\n\n\n\n\n\n\n\n\n\n\n  bla bla.\n\n  <a href="../../assets/Test.pdf">Notre grille tarifaire (PDF)</a>\n\n  <iframe frameborder="0" width="90%" height="60%" src="../../assets/Test.pdf" scrolling="auto"></iframe>\n\n  <p>\n\n    lien vers site <a href="http://ionicframework.com/docs/v2">docs</a> will be your guide.\n\n  </p>\n\n  <p>interpolation de propriété {{1+1}}</p>\n\n  <p>{{titre}}+{{firsName}}</p>\n\n  <hr />\n\n\n\n<br>\n\n<input type="text" name="" id="" [(ngModel)]="newItem.reference">\n\n<input type="text" name="" id="" [(ngModel)]="newItem.name">\n\n<input type="text" name="" id="" [(ngModel)]="newItem.state">\n\n\n\n<!--   utile pour la maintenance -->\n\n<p> {{newItem |json}}</p>\n\n\n\n\n\n<!--   câblage de l\'évenement sur une méthode du composant -->\n\n<button (click)="creationObjet()">Ajouter un objet</button>\n\n<!--   itération sur une collection -->\n\n  <div\n\n   *ngFor="let item of collection">\n\n  <p> Référense: {{item.reference}}</p>\n\n  <p> nom: {{item.name}}</p>\n\n  <p> state: {{item.state}}</p>\n\n\n\n  <!--   définition de l\'objet $event -->\n\n  <a href="" id="{{item.reference}}" (click)="getDetails($event, \'test\')">vers getDetails</a>\n\n\n\n  <!--   affichage conditionner -->\n\n  <p class="state-0" *ngIf="0==item.state">A faire </p>\n\n  <p class="state-1" *ngIf="1==item.state">En cours </p>\n\n  <p class="state-2"*ngIf="2==item.state">fait </p>\n\n  <hr />\n\n </div>\n\n  \n\n\n\n  <!--   cible du binding -->\n\ndom: <input type="texte" [value]="name">\n\nattr:<input type="texte" value="{{name}}">\n\n<p [innerText]= "name"></p>\n\n<p>{{name}}</p>\n\n\n\n <!--   cible vue vers model -->\n\ndouble binding <input type="text"[(ngModel)]=name>\n\n<button (click)="doClick()">appel méthode dans fichier .ts</button>\n\n<button ion-button id="btn" >click</button>\n\n</ion-content>\n\n'/*ion-inline-end:"C:\Users\eea\Documents\GitHub\01 vue\02_V2B_ionic_start_V1\src\pages\home\home.html"*/
+            selector: 'page-home',template:/*ion-inline-start:"C:\Users\eea\Documents\GitHub\01 vue\02_V2B_ionic_start_V1\src\pages\home\home.html"*/'<ion-header>\n\n  <ion-navbar>\n\n    <ion-title>\n\n      Ionic Blank\n\n    </ion-title>\n\n  </ion-navbar>\n\n</ion-header>\n\n\n\n<ion-content padding>\n\n\n\n    <div>\n\n        <label>PDF src</label>\n\n        <input type="text" placeholder="PDF src" [(ngModel)]="pdfSrc">\n\n    </div>\n\n    <pdf-viewer [src]="pdfSrc" \n\n                [render-text]="true"\n\n                [render-text]="true"\n\n                [show-all]="true"\n\n                [page]="2"\n\n                [stick-to-page]="true"\n\n                [autoresize]="true"\n\n                [original-size]="true"\n\n                [zoom]="0.4"\n\n                style="display: block;"\n\n    ></pdf-viewer>\n\n\n\n\n\n\n\n\n\n\n\n\n\n  bla bla.\n\n  <a href="../../assets/Test.pdf">Notre grille tarifaire (PDF)</a>\n\n  <iframe frameborder="0" width="90%" height="60%" src="../../assets/Test.pdf" scrolling="auto"></iframe>\n\n  <p>\n\n    lien vers site <a href="http://ionicframework.com/docs/v2">docs</a> will be your guide.\n\n  </p>\n\n  <p>interpolation de propriété {{1+1}}</p>\n\n  <p>{{titre}}+{{firsName}}</p>\n\n  <hr />\n\n\n\n<br>\n\n<input type="text" name="" id="" [(ngModel)]="newItem.reference">\n\n<input type="text" name="" id="" [(ngModel)]="newItem.name">\n\n<input type="text" name="" id="" [(ngModel)]="newItem.state">\n\n\n\n<!--   utile pour la maintenance -->\n\n<p> {{newItem |json}}</p>\n\n\n\n\n\n<!--   câblage de l\'évenement sur une méthode du composant -->\n\n<button (click)="creationObjet()">Ajouter un objet</button>\n\n<!--   itération sur une collection -->\n\n  <div\n\n   *ngFor="let item of collection">\n\n  <p> Référense: {{item.reference}}</p>\n\n  <p> nom: {{item.name}}</p>\n\n  <p> state: {{item.state}}</p>\n\n\n\n  <!--   définition de l\'objet $event -->\n\n  <a href="" id="{{item.reference}}" (click)="getDetails($event, \'test\')">vers getDetails</a>\n\n\n\n  <!--   affichage conditionner -->\n\n  <p class="state-0" *ngIf="0==item.state">A faire </p>\n\n  <p class="state-1" *ngIf="1==item.state">En cours </p>\n\n  <p class="state-2"*ngIf="2==item.state">fait </p>\n\n  <hr />\n\n </div>\n\n  \n\n\n\n  <!--   cible du binding -->\n\ndom: <input type="texte" [value]="name">\n\nattr:<input type="texte" value="{{name}}">\n\n<p [innerText]= "name"></p>\n\n<p>{{name}}</p>\n\n\n\n <!--   cible vue vers model -->\n\ndouble binding <input type="text"[(ngModel)]=name>\n\n<button (click)="doClick()">appel méthode dans fichier .ts</button>\n\n<button ion-button id="btn" >click</button>\n\n</ion-content>\n\n'/*ion-inline-end:"C:\Users\eea\Documents\GitHub\01 vue\02_V2B_ionic_start_V1\src\pages\home\home.html"*/
         }),
-        __metadata("design:paramtypes", [typeof (_a = typeof __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["d" /* NavController */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["d" /* NavController */]) === "function" && _a || Object])
+        __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_1_ionic_angular__["d" /* NavController */]])
     ], HomePage);
     return HomePage;
-    var _a;
 }()); //fin constructeur
 
 //# sourceMappingURL=home.js.map
 
 /***/ }),
 
-/***/ 340:
+/***/ 362:
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
 Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser_dynamic__ = __webpack_require__(341);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__app_module__ = __webpack_require__(345);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser_dynamic__ = __webpack_require__(363);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__app_module__ = __webpack_require__(367);
 
 
 Object(__WEBPACK_IMPORTED_MODULE_0__angular_platform_browser_dynamic__["a" /* platformBrowserDynamic */])().bootstrapModule(__WEBPACK_IMPORTED_MODULE_1__app_module__["a" /* AppModule */]);
@@ -137,18 +136,18 @@ Object(__WEBPACK_IMPORTED_MODULE_0__angular_platform_browser_dynamic__["a" /* pl
 
 /***/ }),
 
-/***/ 345:
+/***/ 367:
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppModule; });
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__ = __webpack_require__(41);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__ = __webpack_require__(46);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_core__ = __webpack_require__(1);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_ionic_angular__ = __webpack_require__(88);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__ionic_native_splash_screen__ = __webpack_require__(242);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__ionic_native_status_bar__ = __webpack_require__(247);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5__app_component__ = __webpack_require__(388);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_6__pages_home_home__ = __webpack_require__(248);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_ionic_angular__ = __webpack_require__(96);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__ionic_native_splash_screen__ = __webpack_require__(254);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__ionic_native_status_bar__ = __webpack_require__(259);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5__app_component__ = __webpack_require__(410);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_6__pages_home_home__ = __webpack_require__(260);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_7_ng2_pdf_viewer__ = __webpack_require__(692);
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
@@ -199,16 +198,16 @@ var AppModule = /** @class */ (function () {
 
 /***/ }),
 
-/***/ 388:
+/***/ 410:
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return MyApp; });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(1);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_ionic_angular__ = __webpack_require__(88);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__ionic_native_status_bar__ = __webpack_require__(247);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__ionic_native_splash_screen__ = __webpack_require__(242);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__pages_home_home__ = __webpack_require__(248);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_ionic_angular__ = __webpack_require__(96);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__ionic_native_status_bar__ = __webpack_require__(259);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__ionic_native_splash_screen__ = __webpack_require__(254);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__pages_home_home__ = __webpack_require__(260);
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -280,5 +279,5 @@ var MyApp = /** @class */ (function () {
 
 /***/ })
 
-},[340]);
+},[362]);
 //# sourceMappingURL=main.js.map

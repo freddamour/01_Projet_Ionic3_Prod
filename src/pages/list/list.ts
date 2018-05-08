419bb3c66361325477d4f36b6d00fe794cd6457d
freddamour <fdamour121@gmail.com>
2018-05-09 00:32:36 +0200

    [WooCommerce] -05.012 Ajout Slide avec button et page sLide

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -3,14 +3,6 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 
 
-
-/**
- * Generated class for the ListPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-list',
